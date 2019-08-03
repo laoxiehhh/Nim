6d8f40ae447b7f89b5fe0745ef0e818d18043736
xiexuandong <849351825@qq.com>
2019-08-03 16:39:38 +0800

    feat: Nim 仓库初始化

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Nim />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nim from "./components/Nim";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Nim
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Nim.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <div class="section">
+      <label for="name1">姓名1：</label>
+      <input type="text" v-model="name1" id="name1" />
+    </div>
+    <div class="section">
+      <label for="name2">姓名2：</label>
+      <input type="text" v-model="name2" id="name2" />
+    </div>
+    <div class="section">
+      <label for="name3">姓名3：</label>
+      <input type="text" v-model="name3" id="name3" />
+    </div>
+    <div class="section">
+      <label for="name4">姓名4：</label>
+      <input type="text" v-model="name4" id="name4" />
+    </div>
+    <div class="section">
+      <label for="name5">姓名5：</label>
+      <input type="text" v-model="name5" id="name5" />
+    </div>
+    <div class="section">
+      <label for="name6">姓名6：</label>
+      <input type="text" v-model="name6" id="name6" />
+    </div>
+    <div class="section">
+      <button v-on:click="handleClick">提交</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name1: "",
+      name2: "",
+      name3: "",
+      name4: "",
+      name5: "",
+      name6: ""
+    };
+  },
+  methods: {
+    handleClick() {
+      const name1 = this.name1;
+      console.log(name1);
+    }
+  }
+};
+</script>
+
+
+<style>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  flex-direction: column;
+}
+.section {
+  height: 60px;
+  width: 300px;
+}
+</style>
